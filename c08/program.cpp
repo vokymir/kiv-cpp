22221f8bc941a63596f90386f77f91f801e704e8
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-13 18:36:32 +0100

    c08 vol.9 deferred

--- c08/program.cpp
+++ c08/program.cpp
@@ -8,13 +8,20 @@
 int main() {
   int local_result = 0;
 
-  auto fut = std::async(std::launch::async, []() -> int {
+  // async vs deferred
+  auto fut = std::async(std::launch::deferred, []() -> int {
+    std::cout << "ASYNC START" << std::endl;
+
     int async_result = 0;
 
     for (int i = 0; i < 1000000; i++) {
       async_result *= 2;
       async_result += 1;
     }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+
+    std::cout << "ASYNC DONE" << std::endl;
 
     return async_result;
   });
@@ -23,6 +30,8 @@ int main() {
     local_result *= 3;
     local_result -= 10;
   }
+
+  std::cout << "LOCAL DONE" << std::endl;
 
   int global_result = local_result + fut.get();
 
