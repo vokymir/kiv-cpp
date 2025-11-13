deac229b003d1a5c42487136f1104b2bc5771c1b
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-13 18:10:46 +0100

    c08 vol.2 vytezovani vsech vlakem

--- c08/program.cpp
+++ c08/program.cpp
@@ -2,8 +2,10 @@
 #include <thread>
 
 void psani_z_vlakna(char znak) {
+  volatile int i = 0;
   while (true) {
-    std::cout << znak;
+    // std::cout << znak;
+    i++;
   }
 }
 
@@ -11,10 +13,16 @@ int main() {
   std::thread a(&psani_z_vlakna, 'X');
   std::thread b(&psani_z_vlakna, 'h');
   std::thread c(&psani_z_vlakna, 'r');
+  std::thread d(&psani_z_vlakna, 'X');
+  std::thread e(&psani_z_vlakna, 'h');
+  std::thread f(&psani_z_vlakna, 'r');
 
   a.join();
   b.join();
   c.join();
+  d.join();
+  e.join();
+  f.join();
 
   return 0;
 }
