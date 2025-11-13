aff6c797d86c121bf2bbedb3dc01ba6d501b17c2
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-13 18:11:21 +0100

    c08 vol.3 main furt neco dela

--- c08/program.cpp
+++ c08/program.cpp
@@ -2,10 +2,8 @@
 #include <thread>
 
 void psani_z_vlakna(char znak) {
-  volatile int i = 0;
   while (true) {
-    // std::cout << znak;
-    i++;
+    std::cout << znak;
   }
 }
 
@@ -13,16 +11,12 @@ int main() {
   std::thread a(&psani_z_vlakna, 'X');
   std::thread b(&psani_z_vlakna, 'h');
   std::thread c(&psani_z_vlakna, 'r');
-  std::thread d(&psani_z_vlakna, 'X');
-  std::thread e(&psani_z_vlakna, 'h');
-  std::thread f(&psani_z_vlakna, 'r');
+
+  psani_z_vlakna('_');
 
   a.join();
   b.join();
   c.join();
-  d.join();
-  e.join();
-  f.join();
 
   return 0;
 }
