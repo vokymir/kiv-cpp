cd6025748c474003053fcf1ccce7f24c5e27dac2
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-13 18:34:00 +0100

    c08 vol.8 async

--- c08/program.cpp
+++ c08/program.cpp
@@ -1,49 +1,32 @@
 #include <condition_variable>
+#include <future>
 #include <iostream>
 #include <mutex>
 #include <string>
 #include <thread>
 
-/*
- * Ted uz to vzdycky zacne pisar.
- * */
-
-std::mutex mtx;
-std::condition_variable cv;
-int vysledek = -1;
-
-void ctenar() {
-  int last = vysledek;
-  while (true) {
-    std::unique_lock<std::mutex> lck(mtx);
-
-    cv.wait(lck, [&last]() { return last != vysledek; });
-
-    std::cout << "MAM: " << vysledek << std::endl;
-
-    cv.notify_one();
-  }
-}
-
-void pisar() {
-
-  for (int i = 0; i < 1000; i++) {
-    std::unique_lock<std::mutex> lck(mtx);
-    vysledek++;
-    std::cout << "VYMYSLEL JSEM: " << vysledek << std::endl;
-
-    cv.notify_one();
-
-    cv.wait(lck);
-  }
-}
-
 int main() {
-  std::thread ct(&ctenar);
-  std::thread pi(&pisar);
+  int local_result = 0;
 
-  pi.join();
-  ct.join();
+  auto fut = std::async(std::launch::async, []() -> int {
+    int async_result = 0;
+
+    for (int i = 0; i < 1000000; i++) {
+      async_result *= 2;
+      async_result += 1;
+    }
+
+    return async_result;
+  });
+
+  for (int i = 0; i < 1000000; i++) {
+    local_result *= 3;
+    local_result -= 10;
+  }
+
+  int global_result = local_result + fut.get();
+
+  std::cout << "VYSLEDEK: " << global_result << std::endl;
 
   return 0;
 }
