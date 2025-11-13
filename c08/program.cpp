2181b83061344d7b8c176361c116a0ef6553cbc9
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-13 18:19:13 +0100

    c08 vol.4 mutex / ne CPP verye

--- c08/program.cpp
+++ c08/program.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
+#include <mutex>
+#include <string>
 #include <thread>
 
-void psani_z_vlakna(char znak) {
+std::mutex mtx;
+
+void psani_z_vlakna(std::string slovo) {
   for (int i = 0; i < 300; i++) {
-    std::cout << znak << std::endl;
+
+    mtx.lock();
+
+    for (auto c : slovo) {
+      std::cout << c;
+    }
+    std::cout << std::endl;
+
+    mtx.unlock();
   }
 }
 
 int main() {
-  std::thread a(&psani_z_vlakna, 'a');
-  std::thread b(&psani_z_vlakna, 'b');
-  std::thread c(&psani_z_vlakna, 'c');
+  std::thread a(&psani_z_vlakna, "ahoj");
+  std::thread b(&psani_z_vlakna, "bojo");
+  std::thread c(&psani_z_vlakna, "coho");
 
-  psani_z_vlakna('_');
+  psani_z_vlakna("TOHO");
 
   a.join();
   std::cout << "===== KONEC A =====" << std::endl;
