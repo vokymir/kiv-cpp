3e889c4c4ba6f2b5026c1bfd504c9ee0323c69db
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-13 18:16:36 +0100

    c08 vol.4 konecne joinujeme

--- c08/program.cpp
+++ c08/program.cpp
@@ -2,21 +2,24 @@
 #include <thread>
 
 void psani_z_vlakna(char znak) {
-  while (true) {
-    std::cout << znak;
+  for (int i = 0; i < 300; i++) {
+    std::cout << znak << std::endl;
   }
 }
 
 int main() {
-  std::thread a(&psani_z_vlakna, 'X');
-  std::thread b(&psani_z_vlakna, 'h');
-  std::thread c(&psani_z_vlakna, 'r');
+  std::thread a(&psani_z_vlakna, 'a');
+  std::thread b(&psani_z_vlakna, 'b');
+  std::thread c(&psani_z_vlakna, 'c');
 
   psani_z_vlakna('_');
 
   a.join();
+  std::cout << "===== KONEC A =====" << std::endl;
   b.join();
+  std::cout << "===== KONEC B =====" << std::endl;
   c.join();
+  std::cout << "===== KONEC C =====" << std::endl;
 
   return 0;
 }
