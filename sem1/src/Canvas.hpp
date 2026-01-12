1b77952710cc69c035ae0f016f49bc14482fd5d6
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-01-12 10:58:58 +0100

    init sem1

---
+++ sem1/src/Canvas.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Shape.hpp"
+#include <memory>
+#include <vector>
+
+// Canvas to which any Shape could be added.
+// If any transformation is performed, its effects are applied only to currently
+// existing Shapes. Newly added Shapes will be added into new "coordinates
+// system".
+class Canvas {
+private:
+  int width_;
+  int height_;
+  std::vector<std::unique_ptr<Shape>> shapes_;
+
+public:
+  // Create new canvas with the dimensions of any given width and height.
+  // width and height are not-zero positive integers
+  Canvas(int width, int height);
+
+  // TRANSFORMATIONS
+
+  // Move the canvas for x and y.
+  // x and y are not constrained integer.
+  void translate(int x, int y);
+  // Rotate the canvas with the center of rotation (x,y) for angle a.
+  // x and y are not constrained integer.
+  // a is not constrained angle in degree
+  void rotate(int x, int y, float a);
+  // Scale the canvas with the center of scaling (x,y) for factor f.
+  // x and y are not constrained integer.
+  // f is not-zero scale factor
+  void scale(int x, int y, float f);
+
+  // SHAPES
+
+  // Add any shape to the canvas.
+  void add_shape(std::unique_ptr<Shape> s);
+};
---
+++ sem1/src/Shape.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+class Shape {
+public:
+  void rotate();
+};
