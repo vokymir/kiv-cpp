b611e96985853d8f02632a9b154c62a865e412cf
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-01-12 12:48:19 +0100

    make it compile

--- sem1/src/Shape.hpp
+++ sem1/src/Shape.hpp
@@ -6,16 +6,19 @@ class Shape {
 public:
   // Apply translation for x and y.
   // x and y are not constrained integer.
-  virtual void translate(int x, int y);
+  virtual void translate(int x, int y) = 0;
   // Rotate the shape with the center of rotation (x,y) for angle a.
   // x and y are not constrained integer.
   // a is not constrained angle in degree
-  virtual void rotate(int x, int y, float a);
+  virtual void rotate(int x, int y, float a) = 0;
   // Scale the shape with the center of scaling (x,y) for factor f.
   // x and y are not constrained integer.
   // f is not-zero scale factor
-  virtual void scale(int x, int y, float f);
+  virtual void scale(int x, int y, float f) = 0;
 
-  virtual std::string draw_svg();
-  virtual std::string draw_pgm();
+  virtual std::string draw_svg() = 0;
+  virtual std::string draw_pgm() = 0;
+
+  // base-class stuff
+  virtual ~Shape() = default;
 };
