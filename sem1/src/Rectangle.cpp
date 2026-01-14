106275dcf86f7a1159dab9903f96339a379718b2
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-01-14 16:01:06 +0100

    svg rect

--- sem1/src/Rectangle.cpp
+++ sem1/src/Rectangle.cpp
@@ -1,5 +1,8 @@
 
 #include "Rectangle.hpp"
+#include "Point.hpp"
+#include <format>
+#include <string>
 
 void Rectangle::translate(int x, int y) {
   A_.translate(x, y);
@@ -22,6 +25,21 @@ void Rectangle::scale(int x, int y, float f) {
   D_.scale(x, y, f);
 }
 
-std::string Rectangle::draw_svg() { return "SVG rect"; }
+std::string Rectangle::draw_svg() {
+  std::string output;
+
+  auto draw_line = [](const Point &begin, const Point &end) {
+    return std::format(
+        R"(<line x1="{}" y1="{}" x2="{}" y2="{}" stroke="black" />\n)",
+        begin.x(), begin.y(), end.x(), end.y());
+  };
+
+  output += draw_line(A_, B_);
+  output += draw_line(B_, C_);
+  output += draw_line(C_, D_);
+  output += draw_line(D_, A_);
+
+  return output;
+}
 
 std::string Rectangle::draw_pgm() { return "PGM rect"; }
