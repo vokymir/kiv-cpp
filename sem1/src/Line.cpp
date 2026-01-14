8623128f384adc2270ec3b4b2a3c081cba000a22
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-01-14 15:28:21 +0100

    point transformations

--- sem1/src/Line.cpp
+++ sem1/src/Line.cpp
@@ -2,9 +2,20 @@
 #include "Line.hpp"
 #include <string>
 
-void Line::translate(int x, int y) {}
-void Line::rotate(int x, int y, float a) {}
-void Line::scale(int x, int y, float f) {}
+void Line::translate(int x, int y) {
+  p1_.translate(x, y);
+  p2_.translate(x, y);
+}
+
+void Line::rotate(int x, int y, float a) {
+  p1_.rotate(x, y, a);
+  p2_.rotate(x, y, a);
+}
+
+void Line::scale(int x, int y, float f) {
+  p1_.scale(x, y, f);
+  p2_.scale(x, y, f);
+}
 
 std::string Line::draw_svg() { return "svg"; }
 std::string Line::draw_pgm() { return "pgm"; }
--- sem1/src/Point.hpp
+++ sem1/src/Point.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cmath>
+#include <numbers>
 class Point {
 private:
   int x_;
@@ -22,5 +24,36 @@ public:
 
   bool operator==(const Point &other) const {
     return x_ == other.x_ && y_ == other.y_;
+  }
+
+  void translate(int x, int y) {
+    x_ += x;
+    y_ += y;
+  }
+
+  void rotate(int x, int y, float a) {
+    translate(-x, -y);
+
+    float angle = a * std::numbers::pi / 180.0f;
+
+    float old_x = static_cast<float>(x_);
+    float old_y = static_cast<float>(y_);
+
+    float new_x = old_x * std::cos(angle) - old_y * std::sin(angle);
+    float new_y = old_x * std::sin(angle) + old_y * std::cos(angle);
+
+    x_ = static_cast<int>(std::round(new_x));
+    y_ = static_cast<int>(std::round(new_y));
+
+    translate(x, y);
+  }
+
+  void scale(int x, int y, float f) {
+    translate(-x, -y);
+
+    x_ = static_cast<int>(std::round(x_ * f));
+    y_ = static_cast<int>(std::round(y_ * f));
+
+    translate(x, y);
   }
 };
