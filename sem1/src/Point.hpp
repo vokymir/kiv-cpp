02cc92c0b91fd725aa2f94901d7e1fed3583f106
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-01-14 16:23:07 +0100

    fix point math

--- sem1/src/Point.hpp
+++ sem1/src/Point.hpp
@@ -21,7 +21,7 @@ public:
     y_ = y;
   }
 
-  Point() {}
+  Point() : x_(0), y_(0) {}
   Point(int x, int y) : x_(x), y_(y) {}
 
   bool operator==(const Point &other) const {
@@ -34,31 +34,24 @@ public:
   }
 
   void rotate(int x, int y, float a) {
-    translate(-x, -y);
+    int dx = x_ - x;
+    int dy = y_ - y;
 
     float angle = a * std::numbers::pi / 180.0f;
 
-    float old_x = static_cast<float>(x_);
-    float old_y = static_cast<float>(y_);
+    float new_x = dx * std::cos(angle) - dy * std::sin(angle);
+    float new_y = dx * std::sin(angle) + dy * std::cos(angle);
 
-    float new_x = old_x * std::cos(angle) - old_y * std::sin(angle);
-    float new_y = old_x * std::sin(angle) + old_y * std::cos(angle);
-
-    x_ = static_cast<int>(std::round(new_x));
-    y_ = static_cast<int>(std::round(new_y));
-
-    translate(x, y);
+    x_ = new_x + x;
+    y_ = new_y + y;
   }
 
   void scale(int x, int y, float f) {
     if (Util::is_zero(f)) {
       throw std::runtime_error("TODO: scale factor must != 0");
     }
-    translate(-x, -y);
 
-    x_ = static_cast<int>(std::round(x_ * f));
-    y_ = static_cast<int>(std::round(y_ * f));
-
-    translate(x, y);
+    x_ = x + (x_ - x) * f;
+    y_ = y + (y_ - y) * f;
   }
 };
