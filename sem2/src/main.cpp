fc67d57f3faa39f8b47b639379fed02ca893d31f
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-01-21 20:02:44 +0100

    test build

--- sem2/src/main.cpp
+++ sem2/src/main.cpp
@@ -2,7 +2,8 @@
 
 int main(int argc, char **argv) {
 
-  MPInt::MPInt<4> number;
+  MPInt::MPInt<4> number("123456789012345678901234567890");
+  MPInt::MPInt<MPInt::Unlimited> number2(1234567890);
 
   return 0;
 }
