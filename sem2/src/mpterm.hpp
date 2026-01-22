efae5df398f0ad3629a5aa39c9d633d5bccc7f4d
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-01-22 21:43:41 +0100

    operator&stuff

--- sem2/src/mpterm.hpp
+++ sem2/src/mpterm.hpp
@@ -10,6 +10,7 @@
 #include <readline/readline.h>
 #include <stdexcept>
 #include <string>
+#include <string_view>
 
 namespace MPTerm {
 
@@ -47,11 +48,51 @@ public:
   std::size_t size() const { return bank_.size(); }
 };
 
+// represents any supported operator AND invalid
+enum class Operator { Add, Sub, Mul, Div, Fct, Invalid };
+
+// convert operator to char
+constexpr char to_char(Operator op) {
+  switch (op) {
+  case Operator::Add:
+    return '+';
+  case Operator::Sub:
+    return '-';
+  case Operator::Mul:
+    return '*';
+  case Operator::Div:
+    return '/';
+  case Operator::Fct:
+    return '!';
+
+  case Operator::Invalid:
+    return '?';
+  }
+}
+
+// get operator from char
+constexpr Operator from_char(char ch) {
+  switch (ch) {
+  case '+':
+    return Operator::Add;
+  case '-':
+    return Operator::Sub;
+  case '*':
+    return Operator::Mul;
+  case '/':
+    return Operator::Div;
+  case '!':
+    return Operator::Fct;
+  }
+  return Operator::Invalid;
+}
+
 } // namespace _detail
 
 template <std::size_t P>
   requires(P >= 4 || P == MPInt::Unlimited)
 class MPTerm {
+
 private:
   _detail::Bank<P, 5> bank_;
   bool running = false;
@@ -59,9 +100,12 @@ private:
 public:
   // run the MP Terminal
   void run() {
-    std::cout << "Welcome to Multiple Precision Terminal. Current precision is "
-              << ((P == 0) ? "infinite" : std::to_string(P)) << " bytes."
-              << std::endl;
+    std::cout
+        << "Welcome to Multiple Precision Terminal.\nCurrent precision is "
+        << ((P == 0) ? "infinite" : std::to_string(P))
+        << " bytes.\nWrite simple mathematical expression with at most one "
+           "operation from +.-,*,/,! or the command 'bank' or 'exit'."
+        << std::endl;
 
     running = true;
     while (running) {
@@ -84,6 +128,8 @@ public:
   }
 
 private:
+  // determine what to do based on input, test all commands and if that fail try
+  // expression
   void handle_input(const std::string &input) {
     if (input == "bank") {
       cmd_show_bank();
@@ -96,15 +142,67 @@ private:
     }
   }
 
+  // process command 'bank' and show bank in the terminal
   void cmd_show_bank() {
     for (std::size_t i = 0; i < bank_.size(); ++i) {
       std::cout << "$" << std::to_string(i) << " " << bank_.get(i) << "\n";
     }
   }
 
+  // process command 'exit' and exit the terminal
   void cmd_exit() { running = false; }
 
-  void cmd_expression(const std::string &input) {}
+  void cmd_expression(const std::string &input) {
+    auto op = get_operator(input);
+    if (op == _detail::Operator::Invalid) {
+      std::cout << "Invalid input." << std::endl;
+      return;
+    }
+
+    std::size_t op_pos = input.find(_detail::to_char(op));
+    if (op_pos == std::string::npos) {
+      std::cout << "Invalid operator." << std::endl;
+      return;
+    }
+
+    auto op1 = get_first_operand(input, op_pos);
+
+    if (op == _detail::Operator::Fct) {
+      process_operation(op1, op);
+      return;
+    }
+
+    auto op2 = get_second_operand(input, op_pos);
+
+    process_operation(op1, op, op2);
+  }
+
+  // get the first operator in the string input by comparing all chars (from
+  // left) to operators and wait until its not invalid
+  _detail::Operator get_operator(const std::string &input) {
+    _detail::Operator op = _detail::Operator::Invalid;
+
+    for (std::size_t i = 0; i < input.size(); ++i) {
+      op = _detail::from_char(input[i]);
+
+      if (op != _detail::Operator::Invalid) {
+        break;
+      }
+    }
+
+    return op;
+  }
+
+  std::string_view get_first_operand(const std::string &input,
+                                     std::size_t op_pos) {}
+
+  std::string_view get_second_operand(const std::string &input,
+                                      std::size_t op_pos) {}
+
+  void process_operation(const std::string_view &op1, _detail::Operator op) {}
+
+  void process_operation(const std::string_view &op1, _detail::Operator op,
+                         const std::string_view &op2) {}
 };
 
 } // namespace MPTerm
