72bdeafeb9a76f3ef6922f7ce267af037dbeded1
Vokymir <22869857+vokymir@users.noreply.github.com>
2026-01-24 13:45:19 +0100

    mpterm comments

--- sem2/src/mpterm.hpp
+++ sem2/src/mpterm.hpp
@@ -35,8 +35,11 @@
 #include <string>
 #include <string_view>
 
+// Implementation of Multiple Precision Terminal is in class MPTerm.
 namespace MPTerm {
 
+// All implementation details are hidden here not to be exposed to public API of
+// namespace directly.
 namespace _detail {
 
 // bank for MPInt numbers with precision P, where atmost N numbers are stored
@@ -48,8 +51,7 @@ private:
   std::deque<MPInt::MPInt<P>> bank_;
 
 public:
-  // push new number to bank - reordering of numbers happens & ensure size N is
-  // in place
+  // push new number to bank - reordering of numbers happens & ensure max size N
   void push(MPInt::MPInt<P> &&number) {
     bank_.emplace_front(std::move(number));
 
@@ -58,9 +60,9 @@ public:
     }
   }
 
-  // return copy of number at given index
+  // return const reference of number at given index
   // WARN: first number is at index 1
-  MPInt::MPInt<P> get_copy(std::size_t index) const {
+  const MPInt::MPInt<P> &get_const(std::size_t index) const {
     index -= 1; // convert to standard 0-based indexing
     if (index >= bank_.size()) {
       throw std::range_error("Access to bank at invalid index");
@@ -69,14 +71,11 @@ public:
     return bank_[index];
   }
 
-  // return const reference of number at given index
-  const MPInt::MPInt<P> &get_const(std::size_t index) const {
-    index -= 1;
-    if (index >= bank_.size()) {
-      throw std::range_error("Access to bank at invalid index");
-    }
-
-    return bank_[index];
+  // return copy of number at given index
+  // WARN: first number is at index 1
+  MPInt::MPInt<P> get_copy(std::size_t index) const {
+    const auto &ref = get_const(index);
+    return MPInt::MPInt<P>(ref);
   }
 
   // return size of bank
@@ -87,7 +86,7 @@ public:
 enum class Operator { Add, Sub, Mul, Div, Fct, Invalid };
 
 // convert operator to char
-constexpr char to_char(Operator op) {
+constexpr char op_to_char(Operator op) {
   switch (op) {
   case Operator::Add:
     return '+';
@@ -109,7 +108,7 @@ constexpr char to_char(Operator op) {
 }
 
 // get operator from char
-constexpr Operator from_char(char ch) {
+constexpr Operator op_from_char(char ch) {
   switch (ch) {
   case '+':
     return Operator::Add;
@@ -127,14 +126,20 @@ constexpr Operator from_char(char ch) {
 
 } // namespace _detail
 
+// Multiple Precision Terminal with the precision specified as template
+// parameter P. Uses MPInt numbers under the hood and provides the typical
+// terminal functionality.
 template <std::size_t P>
   requires(P >= 4 || P == MPInt::Unlimited)
 class MPTerm {
-
+  // ===== Variables =====
 private:
+  // storage for last 5 numbers
   _detail::Bank<P, 5> bank_;
+  // flag which commands can manipulate
   bool running = false;
 
+  // ===== Methods =====
 public:
   // run the MP Terminal
   void run() {
@@ -165,6 +170,7 @@ public:
               << std::endl;
   }
 
+  // ===== Methods for handling input =====
 private:
   // determine what to do based on input, test all commands and if that fail try
   // expression
@@ -179,6 +185,8 @@ private:
       cmd_expression(input);
     }
   }
+
+  // === Commands ===
 
   // process command 'bank' and show bank in the terminal
   void cmd_show_bank() {
@@ -196,9 +204,12 @@ private:
 
   // process mathematical expression
   void cmd_expression(const std::string &input) {
+    // explanation of regex:
     // [spaces][$]number[spaces]operator[spaces][$]number[spaces]
+    // where second number is optional (because of factorial)
     static const std::regex expression(
         R"(^\s*([$\d]+)\s*([+\-*/!])\s*([$\d]*)\s*$)");
+    // (if there is $$1, it will fail)
 
     std::smatch match;
     if (!std::regex_match(input, match, expression)) {
@@ -208,7 +219,7 @@ private:
 
     // get operands & operator
     std::string op1 = match[1].str();
-    _detail::Operator op = _detail::from_char(match[2].str()[0]);
+    _detail::Operator op = _detail::op_from_char(match[2].str()[0]);
     std::string op2 = match[3].str();
 
     // early fail on missing 2nd operator
@@ -217,11 +228,12 @@ private:
       return;
     }
 
-    // do the maths - may throw for variety of reasons (one of which is
-    // invalid formatting)
+    // do the maths - may throw for variety of reasons
     try {
       process_operation(op1, op, op2);
 
+      // overflow is handled specifically according to the assignment
+      // NOTE: won't prematurely return as other catches in this try
     } catch (const MPInt::Overflow_Error &e) {
       std::cout << "Operation caused overflow, because its result couldn't fit "
                    "inside terminal precision ("
@@ -229,13 +241,16 @@ private:
                 << " bytes).\nResult: " << e.unlimited_value()
                 << "\nSaved as number with terminal precision." << std::endl;
 
+      // store the number in terminal precision - using explicit constructor
       bank_.push(MPInt::MPInt<P>(e.unlimited_value()));
 
+      // generic exception is just de facto ignored
     } catch (const std::exception &e) {
       std::cerr << e.what() << std::endl;
-      std::cout << "Invalid input" << std::endl;
+      std::cout << "Invalid input." << std::endl;
       return;
 
+      // weird exception is just catched
     } catch (...) {
       std::cout << "Unknown error, presumably invalid input." << std::endl;
       return;
@@ -246,9 +261,9 @@ private:
   }
 
   // Get the actual numbers and do the math on them.
-  // may throw on any get_operand() mismatch
-  // and on any overflow
-  // and on any invalid operator
+  // The second operand may be empty, if operator is factorial.
+  //
+  // May throw on any overflow and on any invalid operator.
   void process_operation(const std::string_view &op1, _detail::Operator op,
                          const std::string_view &op2) {
     MPInt::MPInt<P> result{};
@@ -256,7 +271,7 @@ private:
 
     // factorial is a special case - requires only 1 operand
     if (op == _detail::Operator::Fct) {
-      result = operand1;
+      result = std::move(operand1); // just so result is semantic
       result.factorial();
       bank_.push(std::move(result));
       return;
@@ -264,6 +279,7 @@ private:
 
     MPInt::MPInt<P> operand2 = get_operand(op2);
 
+    // uses the operator overloads on MPInt
     switch (op) {
     case _detail::Operator::Add:
       result = operand1 + operand2;
@@ -287,8 +303,13 @@ private:
 
   // get the actual MPInt from str. handle normal number and bank $x
   // may throw if:
-  // - operand is a number, but it's too big for precision
   // - operand is $x and the index is wrong/too big/small
+  // - operand is a number, but it's too big for precision
+  // NOTE: only in cmd_expression is that catched and handled as if the operand
+  // is result. that's OK for positive numbers. might not be OK for negative
+  // (eg. expression is 0 - verylongnumber), but that whole situation is so
+  // rare and user is explicitely warned about error, that it could be
+  // wavehanded
   MPInt::MPInt<P> get_operand(std::string_view str) {
     // normal number
     if (str[0] != '$') {
@@ -297,14 +318,16 @@ private:
 
     // number from bank
     int index = 0;
-    auto idx_view = str.substr(1);
+    auto idx_view = str.substr(1); // skip '$'
 
+    // load number from idx_view to index. if that fails, error is in ec
     auto [ptr, ec] = std::from_chars(idx_view.data(),
                                      idx_view.data() + idx_view.size(), index);
     if (ec != std::errc{} || index <= 0) {
       throw std::invalid_argument("Invalid bank reference.");
     }
 
+    // this needs to be modifiable instance of MPInt, so copy
     return bank_.get_copy(index);
   }
 };
